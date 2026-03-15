d1b436007e515805fe157ce034f40d9a7b92b8e6
himaindla576 <himaindla576@gmail.com>
2026-03-15 16:51:35 +0530

    feat(toprecision):writing js code

---
+++ JavaScript/ToPrecision.js
@@ -0,0 +1,7 @@
+
+function onClickPrecisionCheck(){
+    debugger;
+    DecimalNumber=Number(document.getElementById("txtDecimalNumber").value)
+    PrecisionNumber=Number(document.getElementById("txtPrecisionNumber").value)
+    document.getElementById("pResult").innerHTML= DecimalNumber.toPrecision(PrecisionNumber)
+}
