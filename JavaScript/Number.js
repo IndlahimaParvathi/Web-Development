687a9cd0a657817ebf6e242425b85ce8625ee4e5
himaindla576 <himaindla576@gmail.com>
2026-03-15 15:37:52 +0530

    feat(number):adding demo and defer

--- JavaScript/Number.js
+++ JavaScript/Number.js
@@ -1,5 +1,15 @@
 function onClickNumberChecker(){
     debugger;
-    var result=number(document.getElementById("txtValue").value);
+    var result=Number(document.getElementById("txtValue").value);
     document.getElementById("pResult").innerHTML=result;
 }
+document.getElementById("pDemo").innerHTML = 
+  Number("-10") + "<br>" +
+  Number("-10.33") + "<br>" +
+  Number("10") + "<br>" +
+  Number("10.33") + "<br>" +
+  Number("10 6") + "<br>" +  
+  Number("10 years") + "<br>" +            
+  Number("years 10") + "<br>" +
+  Number(true) + "<br>" +
+  Number(false);
