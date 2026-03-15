9bb7f5fa3382bf41f51453806050515327607017
himaindla576 <himaindla576@gmail.com>
2026-03-15 13:31:35 +0530

    feat(parsefloat.js):adding demo classes

--- JavaScript/ParseFloat.js
+++ JavaScript/ParseFloat.js
@@ -4,5 +4,13 @@ function onClickParseFloatChecker(){
     document.getElementById("pResult").innerHTML=result;
     
 }
+document.getElementById("pDemo").innerHTML = 
+  Number.parseInt("-10") + "<br>" +
+  Number.parseInt("-10.33") + "<br>" +
+  Number.parseInt("10") + "<br>" +
+  Number.parseInt("10.33") + "<br>" +
+  Number.parseInt("10 6") + "<br>" +  
+  Number.parseInt("10 years") + "<br>" +            
+  Number.parseInt("years 10");  
 
 
