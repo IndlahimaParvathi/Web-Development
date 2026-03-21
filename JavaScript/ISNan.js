2bb842d7bddb32216adbe6e11c4707ebbac1c149
himaindla576 <himaindla576@gmail.com>
2026-03-21 12:14:44 +0530

    feat(isnan):buliding js

--- JavaScript/ISNan.js
+++ JavaScript/ISNan.js
@@ -1,5 +1,26 @@
-function onClickNaNChecker(){
+// function onClickNaNChecker(){
+//     debugger;
+//    var  notANumber=document.getElementById("txtnotANumber").value
+//     document.getElementById("pResult").innerHTML=isNaN(notANumber);  
+// }
+
+
+function onClickNanChecker(){
     debugger;
-   var  notANumber=document.getElementById("txtnotANumber").value
-    document.getElementById("pResult").innerHTML=isNaN(notANumber);  
+    var firstValue=document.getElementById("txtfirstvalue").value
+    var secondValue=document.getElementById("txtsecondvalue").value
+    var firstValueisNAN= isNaN(firstValue);
+     var secondValueisNAN= isNaN(secondValue);
+     console.log(firstValueisNAN);
+     console.log(secondValueisNAN);
+     if(!firstValueisNAN && !secondValueisNAN){
+       document.getElementById("pResult").innerHTML=parseInt(firstValue)+parseInt(secondValue);
+
+     }
+
+        else{
+            document.getElementById("pResult").innerHTML=firstValue+" "+secondValue;
+        }
+
+    
 }
