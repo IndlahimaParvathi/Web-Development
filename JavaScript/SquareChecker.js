679938a285f5525d1c3597bcb7944e31451872be
himaindla576 <himaindla576@gmail.com>
2026-03-21 16:44:14 +0530

    buliding html

---
+++ JavaScript/SquareChecker.js
@@ -0,0 +1,5 @@
+function onClickSquareChecker(){
+    debugger;
+   var inputValue= document.getElementById("txtinputvalue").value 
+   document.getElementById("Result").innerHTML=Math.sqrt(inputValue);
+}
