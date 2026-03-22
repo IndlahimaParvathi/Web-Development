ff4774fde222b6ee4067ad5f61df3a58f3a7f72a
himaindla576 <himaindla576@gmail.com>
2026-03-22 13:05:07 +0530

    bug(mathpower): resolving the mistakes

--- JavaScript/MathPower.js
+++ JavaScript/MathPower.js
@@ -1,6 +1,6 @@
 function onClickPowerChecker(){
     debugger;
-    var value=document.getElementById("txtvalue").value
-    var power=document.getElementById("txtpower").value
-    document.getElementById("result").innerHTML=Math.pow(value,power);
+    var value=document.getElementById("txtValue").value
+    var power=document.getElementById("txtPower").value
+    document.getElementById("pResult").innerHTML=Math.pow(value,power);
 }
