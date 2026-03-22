f1d6e51eb5f187965761797c63e897c229e0226d
himaindla576 <himaindla576@gmail.com>
2026-03-22 13:19:45 +0530

    buliding js

---
+++ JavaScript/MathRandom.js
@@ -0,0 +1,6 @@
+function onClickGenerateRandom(){
+    debugger;
+    var sizeNumber= Number(document.getElementById("txtInput").value);
+    var randomNumber =Math.round(Math.random()*Math.pow(10,sizeNumber));
+    document.getElementById("result").innerHTML= "Random Number:" +randomNumber;
+}
