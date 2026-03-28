4ea7a201a1edb190736f96b76abf74e84686dfd0
himaindla576 <himaindla576@gmail.com>
2026-03-28 17:22:38 +0530

    bug the mistakes

--- JavaScript/OrOperation.js
+++ JavaScript/OrOperation.js
@@ -1,5 +1,6 @@
 function onClickOrOperation(){
     debugger;
+    var Number= parseFloat(document.getElementById("txtNumber").value)
     if((Number%7!=0) || (Number%11!=0)){
         document.getElementById("pResult").innerHTML="The number is divisible by 7 and 11.";
         console.log("The number is divisible by 7 and 11. ");
