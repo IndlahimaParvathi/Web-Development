c76c4dfa0a2da46fe750899fcd5d27f0e6be2a3c
himaindla576 <himaindla576@gmail.com>
2026-03-28 17:20:51 +0530

    buliding js

---
+++ JavaScript/OrOperation.js
@@ -0,0 +1,7 @@
+function onClickOrOperation(){
+    debugger;
+    if((Number%7!=0) || (Number%11!=0)){
+        document.getElementById("pResult").innerHTML="The number is divisible by 7 and 11.";
+        console.log("The number is divisible by 7 and 11. ");
+ }
+}
