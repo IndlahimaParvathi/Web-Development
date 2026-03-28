fe881d378545b67af35939d1e295b48d58e70fe8
himaindla576 <himaindla576@gmail.com>
2026-03-28 17:12:12 +0530

    bug the mistakes

--- JavaScript/AndOperation.js
+++ JavaScript/AndOperation.js
@@ -1,5 +1,6 @@
  function onClickAndOperation(){
      if((Number%3==0) && (Number%5==0)){
-        document.getElementById("pResult").innerHTML="The number is divisible by  3and 5.";
-        console.log("The number is divisible 3and 5. ");
+        document.getElementById("pResult").innerHTML="The number is divisible by  3 and 5.";
+        console.log("The number is divisible 3 and 5. ");
+     }
  }
