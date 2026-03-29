d27cff8749b62f07282319878e24685ececa6c1f
himaindla576 <himaindla576@gmail.com>
2026-03-29 13:29:42 +0530

    write in another way

--- JavaScript/IfElseConditions.js
+++ JavaScript/IfElseConditions.js
@@ -1,15 +1,32 @@
+// function onClickAdd(){
+//     debugger;
+//     var number=Number(document.getElementById("txtNumber").value)
+//     if(number%3== 0&& number%7==0){
+//          document.getElementById("result").innerHTML="The number is divisible 3and 7";
+//     }
+//     else {
+//          document.getElementById("result").innerHTML="The number is not divisible 3 and 7.";        
+//     } 
+// }
+
 function onClickAdd(){
     debugger;
     var number=Number(document.getElementById("txtNumber").value)
     if(number%3== 0&& number%7==0){
          document.getElementById("result").innerHTML="The number is divisible 3and 7";
     }
-    else {
-         document.getElementById("result").innerHTML="The number is not divisible 3 and 7.";
-
-    }
-
+    else if(number%3== 0) {
+         document.getElementById("result").innerHTML="The number is  divisible 3 ";        
+    } 
+    else if (number%7==0) {
+        document.getElementById("result").innerHTML="The number is divisible 7.";        
     
+    }
+    else {
+         document.getElementById("result").innerHTML="The number is not divisible 3 and 7.";        
+    
+    }
 }
+
 
 
