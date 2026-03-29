bf73a46ad938f311eda704f85a44c10bc6de0d52
himaindla576 <himaindla576@gmail.com>
2026-03-29 13:15:47 +0530

    rewrite code

--- JavaScript/IfElseConditions.js
+++ JavaScript/IfElseConditions.js
@@ -2,10 +2,10 @@ function onClickAdd(){
     debugger;
     var number=Number(document.getElementById("txtNumber").value)
     if(number%3== 0&& number%7==0){
-         document.getElementById("result").innerHTML="The number is divisible 3.";
+         document.getElementById("result").innerHTML="The number is divisible 3and 7";
     }
     else {
-         document.getElementById("result").innerHTML="The number is divisible 7.";
+         document.getElementById("result").innerHTML="The number is not divisible 3 and 7.";
 
     }
 
