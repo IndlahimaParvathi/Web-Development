ddad1a9a8327c50fd544954db9d64661f58d46d2
himaindla576 <himaindla576@gmail.com>
2026-03-30 21:40:19 +0530

    bug the mistakes

--- JavaScript/IfElseConditions.js
+++ JavaScript/IfElseConditions.js
@@ -16,14 +16,14 @@ function onClickAdd(){
          document.getElementById("result").innerHTML="The number is divisible 3and 7";
     }
     else if(number%3== 0) {
-         document.getElementById("result").innerHTML="The number is  divisible 3 ";        
+         document.getElementById("result").innerHTML="The number is  divisible by 3 ";        
     } 
     else if (number%7==0) {
-        document.getElementById("result").innerHTML="The number is divisible 7.";        
+        document.getElementById("result").innerHTML="The number is divisible by 7.";        
     
     }
     else {
-         document.getElementById("result").innerHTML="The number is not divisible 3 and 7.";        
+         document.getElementById("result").innerHTML="The number is not divisible by 3 and 7.";        
     
     }
 }
