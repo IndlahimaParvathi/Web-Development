cb3382d4b3e2f714e0fb457fedd5dcf0e0264359
himaindla576 <himaindla576@gmail.com>
2026-03-31 21:52:00 +0530

    adding console log

--- JavaScript/AndOperation.js
+++ JavaScript/AndOperation.js
@@ -2,6 +2,6 @@
      var Number= parseFloat(document.getElementById("txtNumber").value)
      if((Number%3==0) && (Number%5==0)){
         document.getElementById("pResult").innerHTML="The number is divisible by  3 and 5.";
-       ("The number is divisible 3 and 5. ");
+       console.log("The number is divisible 3 and 5. ");
      }
  }
