6161363fe9b5ef3b562f7734e7717f778497907e
himaindla576 <himaindla576@gmail.com>
2026-04-10 19:32:03 +0530

    buliding js

---
+++ JavaScript/ParseFloatEvent.js
@@ -0,0 +1,17 @@
+document.getElementById("btnparseFloatCheck").addEventListener("click",function(){
+
+    debugger;
+    var result=parseFloat(document.getElementById("txtValue").value);
+    document.getElementById("pResult").innerHTML=result;
+    
+});
+document.getElementById("pDemo").innerHTML = 
+  Number.parseInt("-10") + "<br>" +
+  Number.parseInt("-10.33") + "<br>" +
+  Number.parseInt("10") + "<br>" +
+  Number.parseInt("10.33") + "<br>" +
+  Number.parseInt("10 6") + "<br>" +  
+  Number.parseInt("10 years") + "<br>" +            
+  Number.parseInt("years 10");  
+
+
